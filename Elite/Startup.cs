78bd38c255e866db78a9bf71da9ab6d693117ad9
muhammadbasheerrajeh <muhammadbasheerrajeh@gmail.com>
2022-01-31 00:47:22 +0200

    add session

--- Elite/Startup.cs
+++ Elite/Startup.cs
@@ -43,6 +43,13 @@ namespace Elite
 
             services.AddScoped<IUnitOfWork, UnitOfWork>();
 
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(30);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
+
             services.AddControllersWithViews().AddNewtonsoftJson().AddRazorRuntimeCompilation();
             services.AddControllers().AddNewtonsoftJson(x => x.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
 
@@ -98,6 +105,8 @@ namespace Elite
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+
+            app.UseSession();
 
             app.UseRouting();
 
