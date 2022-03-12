99ac57eb8725d3b01a089e39a811e651097d078a
BasheerRajeh <MuhammadBasheerRajeh@hotmail.com>
2022-03-12 02:38:00 +0200

    change in HotelContext

--- Elite.Models/Migrations/20220312003101_change customerId to UserId from ApplicationUser.Designer.cs
+++ Elite.Models/Migrations/20220312003101_change customerId to UserId from ApplicationUser.Designer.cs
@@ -4,14 +4,16 @@ using Elite.AppDbContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Elite.AppDbContext.Migrations
 {
     [DbContext(typeof(HotelContext))]
-    partial class HotelContextModelSnapshot : ModelSnapshot
+    [Migration("20220312003101_change customerId to UserId from ApplicationUser")]
+    partial class changecustomerIdtoUserIdfromApplicationUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Elite.Models/Migrations/20220312003101_change customerId to UserId from ApplicationUser.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Elite.AppDbContext.Migrations
+{
+    public partial class changecustomerIdtoUserIdfromApplicationUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CustomerId",
+                table: "Reservation");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Reservation",
+                maxLength: 450,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservation_UserId",
+                table: "Reservation",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reservation_AspNetUsers_UserId",
+                table: "Reservation",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reservation_AspNetUsers_UserId",
+                table: "Reservation");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Reservation_UserId",
+                table: "Reservation");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Reservation");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CustomerId",
+                table: "Reservation",
+                type: "nvarchar(450)",
+                maxLength: 450,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
